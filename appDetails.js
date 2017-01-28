11b460e56008ac99984c2cb25b66da2752c7dcbd
Alistair Madden <Alistair.John.Madden@gmail.com>
2017-01-28 22:04:56 +0000

    Correct noob mistake

---
+++ appDetails.js
@@ -0,0 +1,7 @@
+var appDetails = {dbDetails:{host: "myeusql.dur.ac.uk",
+    user: "hnvp67",
+    password: "hack_4_fun",
+    insecureAuth: true,
+    database: "Phnvp67_safety_app"}};
+
+module.exports = appDetails;
