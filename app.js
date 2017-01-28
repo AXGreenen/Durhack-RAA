c4b26ad1f0cde728517510afe71999ca6b4bf0d5
Alistair Madden <Alistair.John.Madden@gmail.com>
2017-01-28 16:12:36 +0000

    Add cover page
    Move index.html into Public directory so it can be served.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'Public')));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -16,5 +19,4 @@ app.get('/lighting', function(req, res){
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-
 });
