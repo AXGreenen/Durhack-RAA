05780ea4e29cf53e107867ad480e668009f6ffab
Sora <steenvoortl@aol.com>
2017-01-29 01:13:35 +0000

    add heatmap

---
+++ Public/Lighting2/lighting.js
@@ -0,0 +1,63 @@
+var map;
+var heatmap;
+var hmdata;
+
+function putlightdatapoint(latitude,longitude) {
+    hmdata.push(new google.maps.LatLng(latitude,longitude));
+    // var lightingicon = 'url_of_the_lighting_image'
+   // var marker = new google.maps.Marker({
+     //   position: {lat: latitude, lng: longitude},
+     //   map: map
+        //type:lighting
+   // });
+
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+}
+
+      function initMap() {
+        hmdata = [];
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 54.7732, lng: -1.5764},
+          zoom: 15,
+          mapTypeId:'roadmap'
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: [],
+            //data: null,
+            map: map,
+            radius: 15
+	});
+
+    google.maps.event.addListener(map, 'bounds_changed', function () {
+        var topLeft;
+        var bottomRight;
+        var googleCoords = map.getBounds();
+        topLeft = [googleCoords.f.b, googleCoords.b.b];
+        bottomRight = [googleCoords.f.f, googleCoords.b.f];
+
+        var httpRequest = new XMLHttpRequest();
+
+        httpRequest.onreadystatechange = function() {
+	    hmdata = [];
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                console.log(httpRequest.status);
+                var jsonResponse = JSON.parse(httpRequest.responseText);
+                for(var i in jsonResponse) {
+                    putlightdatapoint(parseFloat(jsonResponse[i].latitude), parseFloat(jsonResponse[i].longitude));
+                }
+		heatmap.set('data', hmdata);
+            }
+        };
+
+        httpRequest.open('GET', 'http://localhost:3000/api/lighting?t=' + topLeft[0] + '&b=' + bottomRight[0] + "&l=" + topLeft[1] + "&r=" + bottomRight[1], true);
+        httpRequest.send();
+    });
+
+
+
+}
+
